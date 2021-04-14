96c840b367dbef14039820cff6b58af726fe5db4
Pirlobo <bonguyens2001@gmail.com>
2021-04-14 04:26:21 -0700

    first commit

--- src/main/java/com/bezkoder/springjwt/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bezkoder/springjwt/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.bezkoder.springjwt.exceptions;
 
 import java.util.Date;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,34 +8,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import com.bezkoder.springjwt.payload.response.MessageResponse;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	// handling specific exception
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request){
-		ErrorDetails errorDetails = 
-				new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+	public ResponseEntity<?> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 	}
+
 	@ExceptionHandler(NullPointerException.class)
-	public ResponseEntity<?> nullPointerExceptionHandling(NullPointerException exception, WebRequest request){
-		ErrorDetails errorDetails = 
-				new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+	public ResponseEntity<?> nullPointerExceptionHandling(NullPointerException exception, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 
-	 @ExceptionHandler(MaxUploadSizeExceededException.class)
-	  public ResponseEntity<MessageResponse> handleMaxSizeException(MaxUploadSizeExceededException exc) {
-	    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageResponse("File too large!"));
-	  }
-
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<MessageResponse> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageResponse("File too large!"));
+	}
 
 	// handling global exception
-	
+
 //	@ExceptionHandler(Exception.class)
 //	public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request){
 //		ErrorDetails errorDetails = 
--- src/main/java/com/bezkoder/springjwt/exceptions/NullExceptionHandler.java
+++ src/main/java/com/bezkoder/springjwt/exceptions/NullExceptionHandler.java
@@ -2,8 +2,9 @@ package com.bezkoder.springjwt.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class NullExceptionHandler extends NullPointerException{
+public class NullExceptionHandler extends NullPointerException {
 
 	private static final long serialVersionUID = 1L;
 
